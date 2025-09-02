cfcd841df1351e213a70ca92a4d24006578a84d9
sebastianroldan8 <s.roldan8496@gmail.com>
2025-09-01 19:23:56 -0500

    feat se crea entinda SebastianRoldan

---
+++ src/main/java/com/example/FrankySabado/modelos/SebastianRoldan.java
@@ -0,0 +1,61 @@
+package com.example.FrankySabado.modelos;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
+@Table(name="SebastianRoldan")
+public class SebastianRoldan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nombre", nullable = false)
+    private  String nombre;
+    @Column(name = "fecha", nullable = false, unique = false)
+    private LocalDate fecha;
+    @Column(name = "correo", nullable = false, unique = true)
+    private  String correo;
+
+    public SebastianRoldan() {
+    }
+
+    public SebastianRoldan(Integer id, String correo, LocalDate fecha, String nombre) {
+        this.id = id;
+        this.correo = correo;
+        this.fecha = fecha;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
