c567ff7a6b5423402c5b0ed248357fc9e1c3b2c2
Desarrolladorloco2024 <jhoniercespedes0812@gmail.com>
2025-09-01 19:23:57 -0500

    feat se crea entidad jhonier

---
+++ src/main/java/com/example/FrankySabado/modelos/Jhonier.java
@@ -0,0 +1,64 @@
+package com.example.FrankySabado.modelos;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Jhonier")
+
+public class Jhonier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "fecha")
+    private LocalDate fecha;
+    @Column(name = "correo")
+    private String correo;
+
+    public Jhonier() {
+    }
+
+    public Jhonier(Integer id, String nombre, LocalDate fecha, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.correo = correo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
+
+
