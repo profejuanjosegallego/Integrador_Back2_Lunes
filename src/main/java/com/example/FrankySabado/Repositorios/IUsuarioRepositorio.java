5bf19cbbd3b95fb9cd15e91e1fe97061b9c0b547
Thomas Serna <Tom.serna259@gmail.com>
2025-09-06 00:56:20 -0500

    correcion tabla matricula

---
+++ src/main/java/com/example/FrankySabado/Repositorios/IUsuarioRepositorio.java
@@ -0,0 +1,9 @@
+package com.example.FrankySabado.Repositorios;
+
+import com.example.FrankySabado.modelos.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUsuarioRepositorio extends JpaRepository<Usuario,Integer> {
+}
