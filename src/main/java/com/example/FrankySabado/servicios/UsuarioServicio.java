91efb7ba62b48205263d731923c280c3f66d6103
Juan Peréz Peŕez <lverag28@gmail.com>
2025-09-15 19:42:52 -0500

    ok

--- src/main/java/com/example/FrankySabado/servicios/UsuarioServicio.java
+++ src/main/java/com/example/FrankySabado/servicios/UsuarioServicio.java
@@ -7,6 +7,9 @@ import com.example.FrankySabado.repositorios.IUsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UsuarioServicio {
 
@@ -27,6 +30,46 @@ public class UsuarioServicio {
             throw new Exception("upss fallamos "+error.getMessage());
         }
     }
+
+    //2. Servicio para buscar Todos los usuarios
+    public List<UsuarioGenericoDTO> buscarUsuarios()throws Exception{
+        try{
+            return this.mapa.convertirListaDTO(this.repositorio.findAll());
+        }catch(Exception error){
+            throw new Exception("upss fallamos "+error.getMessage());
+        }
+    }
+
+    //3. Servicio para buscar un usuario si me dan su ID
+    public UsuarioGenericoDTO buscarUsuarioPorId(Integer id)throws Exception{
+        try{
+            Optional<Usuario>usuarioBuscado=this.repositorio.findById(id);
+            if(usuarioBuscado.isPresent()){ //SI SI ESTA
+                return this.mapa.convertirADTO(usuarioBuscado.get());
+            }else{ //SI NO ESTA
+                throw new Exception("Usuario no encontrado");
+            }
+        }catch(Exception error){
+            throw new Exception("upss fallamos "+error.getMessage());
+        }
+    }
+
+    //Buscar por correo
+    public UsuarioGenericoDTO buscarUsuarioPorCorreo(String correo)throws Exception{
+        try{
+            Optional<Usuario>usuarioBuscado=this.repositorio.findByCorreo(correo);
+            if(usuarioBuscado.isPresent()){ //SI SI ESTA
+                return this.mapa.convertirADTO(usuarioBuscado.get());
+            }else{ //SI NO ESTA
+                throw new Exception("Usuario no encontrado");
+            }
+        }catch(Exception error){
+            throw new Exception("upss fallamos "+error.getMessage());
+        }
+    }
+
+
+
 
 
 }
